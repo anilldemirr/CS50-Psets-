f5c1e650779b7da97b95a94dcf4f8481c05bfce7
anilldemirr <118938083+anilldemirr@users.noreply.github.com>
2023-01-11 18:54:46 +0300

    Add files via upload

---
+++ CS502.odev.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <string.h>
+int main()
+{
+int i,adet; 
+char message[20];
+puts("Please Enter Your Message");
+gets(message);
+adet=strlen(message);
+for(i=0;i<adet;i++)
+{
+  message[i]=message[i]-1;
+}
+printf("Message encrypted by caesar:\n");
+printf("%s",message);
+}
