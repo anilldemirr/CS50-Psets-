89afab129b60af246941619241b0e12e1bb193f0
anilldemirr <118938083+anilldemirr@users.noreply.github.com>
2023-01-11 21:26:37 +0300

    Update CS501.odev.c

--- CS501.odev.c
+++ CS501.odev.c
@@ -1,48 +1,33 @@
 #include <stdlib.h>
 #include <stdio.h>
-
+#include <math.h>
 
 int main()
 {
-float paraustu;
-float yirmibes=0.25,onluk=0.10,beslik=0.05,birlik=0.01;
-int yirmibessayisi=0,onluksayisi=0,besliksayisi=0,birliksayisi=0;
-printf("\n\nPara üstü verilecek tutar:");
+   float paraustu;
+    int yirmibessayisi=0,onluksayisi=0,besliksayisi=0,birliksayisi=0;
+printf("\n\nPara ustu verilecek tutari giriniz:");
 scanf("%f",&paraustu);
-while (paraustu>0.01)
-{
-if(paraustu>yirmibes)
-{
-paraustu=paraustu-yirmibes;
-yirmibessayisi++;
-continue;
-}
-if (paraustu>onluk)
-{
-    paraustu=paraustu-onluk;
-    onluksayisi++;
-    continue;
-}
+paraustu=round(paraustu*(100));
 
-if (paraustu>beslik)
-{
-    paraustu=paraustu-beslik;
-    besliksayisi++;
-    continue;
-}
+yirmibessayisi=paraustu/25;
+paraustu=paraustu-(25*yirmibessayisi);
 
-if (paraustu>birlik)
-{
-    paraustu=paraustu-birlik;
-    birliksayisi++;
-    continue;
-}
+onluksayisi=paraustu/10;
+paraustu=paraustu-(10*onluksayisi);
+ 
+ besliksayisi=paraustu/5;
+ paraustu=paraustu-(5*besliksayisi);
 
-}
-printf("%d adet 25 krs\n",yirmibessayisi);
+ birliksayisi=paraustu/1;
+ paraustu=(1*birliksayisi);
+
+
+printf("%d adaet 25 krs\n",yirmibessayisi);
 printf("%d adet 10 krs \n",onluksayisi);
 printf("%d adet 5 krs \n",besliksayisi);
 printf("%d adet 1 krs\n",birliksayisi);
+
 
 
 }
