d1b9491beffb59cc566567ff77eecfb1c1685a2c
anilldemirr <118938083+anilldemirr@users.noreply.github.com>
2023-01-11 20:41:20 +0300

    Update CS501.odev.c

--- CS501.odev.c
+++ CS501.odev.c
@@ -1,15 +1,48 @@
+#include <stdlib.h>
 #include <stdio.h>
-#include <string.h>
-int main(){
-char dizi[20];
-puts("pls enter your message");
-gets(dizi);
-int adet=strlen(dizi);
-for(int i=0;i<adet;i++)
+
+
+int main()
 {
-  dizi[i]=dizi[i]+1;
-  
+float paraustu;
+float yirmibes=0.25,onluk=0.10,beslik=0.05,birlik=0.01;
+int yirmibessayisi=0,onluksayisi=0,besliksayisi=0,birliksayisi=0;
+printf("\n\nlutfen para ustu verilecek tutari giriniz :");
+scanf("%f",&paraustu);
+while (paraustu>0.01)
+{
+if(paraustu>yirmibes)
+{
+paraustu=paraustu-yirmibes;
+yirmibessayisi++;
+continue;
 }
-printf("Message encrypted by caesar:\n");
-printf("%s",dizi);
+if (paraustu>onluk)
+{
+    paraustu=paraustu-onluk;
+    onluksayisi++;
+    continue;
+}
+
+if (paraustu>beslik)
+{
+    paraustu=paraustu-beslik;
+    besliksayisi++;
+    continue;
+}
+
+if (paraustu>birlik)
+{
+    paraustu=paraustu-birlik;
+    birliksayisi++;
+    continue;
+}
+
+}
+printf("%d adet 25 krs\n",yirmibessayisi);
+printf("%d adet 10 krs \n",onluksayisi);
+printf("%d adet 5 krs \n",besliksayisi);
+printf("%d adet 1 krs\n",birliksayisi);
+
+
 }
